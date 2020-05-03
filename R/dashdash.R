2005631e336ba49df72d2f38ad2950fc95e023ff
macartan <macartan@gmail.com>
2020-05-03 09:39:32 +0200

    make maps generic, clean up params, remove packages

--- R/dashdash.R
+++ R/dashdash.R
@@ -5,8 +5,17 @@
 #' Note I had a problem with `Error: isTRUE(gpclibPermitStatus()) is not TRUE` which was solved
 #' with `install.packages('rgeos', type='source')` and `install.packages('rgdal', type='source')`
 #'
+#' @param output_file Path to where ouutput should be written, e.g. "docs/index.html"
+#' @param my_vars dataframe with mapping from variable names to variable families and labels. One ro per variable.
+#' @param my_data dataframe with data on variables listed in `my_vars`. Should contain a `id` variable that connects with units in map files and a `date` variable.
+#' @param my_args dataframe with arguments to customize a dashboard.
 #' @param title Dashboard title
 #' @param subtitle Dashboard subtitle
+#' @param author String, names of authors.
+#' @param map_path String, path to map shapefiles.
+#' @param map_region String, map region.
+#' @param map_layer String, map layer.
+#' @param normalize Logical. Whether to normalize variabels before map plotting.
 #' @importFrom dplyr mutate filter
 #' @importFrom skimr skim
 #' @export
@@ -32,41 +41,58 @@
 #'   stringsAsFactors = FALSE
 #' )
 #'
-#' my_text <- data.frame(
+#' my_args <- data.frame(
 #'   para = "Intro text",
-#'   datanote = "Study specific note about data source"
+#'   datanote = "Study specific note about data source",
+#'   map_path = "c:/temp/shapefiles",
+#'   map_layer = "SLE_adm3",
+#'   map_region =  "NAME_2"
 #'   )
 #'
-#' my_maps <- "c:/temp/shapefiles"
+#'
 #'
 #' dashdash(output_file = "c:/temp/dashtest.html",
 #'          title = "title2",
 #'          subtitle = "subtitle here",
-#'          author   = "D Ash",
 #'          my_data = my_data,
 #'          my_vars = my_vars,
-#'          my_text = my_text,
-#'          my_maps = my_maps)
+#'          my_args = my_args)
 #'
 
 dashdash <- function(output_file,
+                     my_vars,
+                     my_data,
+                     my_args,
                      title = NULL,
                      subtitle = NULL,
                      author = NULL,
-                     my_data,
-                     my_vars,
-                     my_text,
-                     my_maps,
+                     map_path = NULL,
+                     map_region = NULL,
+                     map_layer = NULL,
+                     normalize = NULL,
                      ...){
 
-  if(is.null(title)) title <- my_text$title
+  if(is.null(title)) title <- my_args$title
   if(is.null(title)) title <- "No title provided"
 
-  if(is.null(subtitle)) subtitle <- my_text$subtitle
-  if(is.null(subtitle)) subtitle <- " "
+  if(is.null(subtitle))   subtitle <- my_args$subtitle
 
-  if(is.null(author)) author <- my_text$author
-  if(is.null(author)) author <- " "
+  if(is.null(author))  author <- my_args$author
+
+  if(is.null(map_path)) map_path <- my_args$map_path
+  if(is.null(map_path)) map_path <- "shapefiles"
+
+  if(is.null(map_region)) map_region <- my_args$map_region
+  if(is.null(map_region)) map_layer  <- my_args$map_layer
+
+  if(is.null(normalize)) normalize <- my_args$normalize
+  if(is.null(normalize)) normalize <- FALSE
+
+  # Here -- check if layer is contained in my maps and turn off maps if not
+  map_layer <- my_args$map_layer
+  if(is.null(map_layer)) stop("Map layer should be provided; e.g. `SLE_adm3`")
+
+  map_region <- my_args$region
 
   dashRmd  <- system.file("rmd", "dashdash.Rmd", package = "dashdash")
   childRmd <- system.file("rmd", "child.Rmd", package = "dashdash")
