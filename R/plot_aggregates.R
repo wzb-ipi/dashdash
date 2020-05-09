2d74d4289cdae310d9e8d3cc82fbcb57eafd2a66
Julio Saul Solis Arce <solis@ad.wzb.eu>
2020-05-09 15:59:27 +0200

    slight modifications

--- R/plot_aggregates.R
+++ R/plot_aggregates.R
@@ -11,43 +11,46 @@
 #' @export
 
 
-plot_aggregates <- function(df, my_vars, vars, var_labs, pd = ggplot2::position_dodge(.1))
+plot_aggregates <- function(df, my_vars, vars, var_labs, pd = ggplot2::position_dodge(.1)){
 
-national_plot <- df %>%
+   national_plot <- df %>%
 
-  select(all_of(c("date", vars))) %>%
-  reshape2::melt(id.vars= c("date"))  %>%
-  group_by(date, variable) %>%
-  summarise_all(list(~mean(., na.rm = TRUE), ~sd(., na.rm = TRUE), ~length(.))) %>%
-  mutate(se = sd / sqrt(length)) %>%
+    select(all_of(c("date", vars))) %>%
+    reshape2::melt(id.vars= c("date"))  %>%
+    group_by(date, variable) %>%
+    summarise_all(list(~mean(., na.rm = TRUE), ~sd(., na.rm = TRUE), ~length(.))) %>%
+    mutate(se = sd / sqrt(length)) %>%
 
-  ggplot(aes(x=date, y=mean)) +
-  geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=.1, position=pd) +
-  geom_line(position=pd) +
-  geom_point(position=pd) +
-  facet_wrap(~variable, scales = "free_y",
-             labeller = labeller(variable = var_labs),
-             strip.position = "top") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    ggplot(aes(x=date, y=mean)) +
+    geom_errorbar(aes(ymin=mean-1.96*se, ymax=mean+1.96*se), width=.1, position=pd) +
+    geom_line(position=pd) +
+    geom_point(position=pd) +
+    facet_wrap(~variable, scales = "free_y",
+               labeller = labeller(variable = var_labs),
+               strip.position = "top") +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-if (!is.null(my_vars$min) & !is.null(my_vars$max)){
+  if (!is.null(my_vars$min) & !is.null(my_vars$max)){
 
-  datalist <-  list()
+    datalist <-  list()
 
-  for (index in vars) {
-    selection <- filter(my_vars, variable == index) %>%
-      select(variable, min, max)
-    grid <- expand.grid(vars = selection$variable,
-                        x = c(min(my_data$date), max(my_data$date)),
-                        y = c(selection$min, selection$max))
-    datalist[[index]] <- grid
+    for (index in vars) {
+      selection <- filter(my_vars, variable == index) %>%
+        select(variable, min, max)
+      grid <- expand.grid(vars = selection$variable,
+                          x = c(min(my_data$date), max(my_data$date)),
+                          y = c(selection$min, selection$max))
+      datalist[[index]] <- grid
+
+    }
+
+    ranges <- do.call(rbind, datalist)
+
+    national_plot <- national_plot +  geom_blank(data = ranges, aes(x = x, y = y))
 
   }
 
-  ranges <- do.call(rbind, datalist)
-
-  national_plot <- national_plot +  geom_blank(data = ranges, aes(x = x, y = y))
+  national_plot
 
 }
 
-national_plot
