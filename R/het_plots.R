1201a7f88d3eb6c5c4256aeddfd447812424b1f6
Julio Saul Solis Arce <solis@ad.wzb.eu>
2020-09-16 09:36:11 +0200

    more

--- R/het_plots.R
+++ R/het_plots.R
@@ -90,18 +90,18 @@ het_bar_plot <- function(df, my_vars){
     question_label <- split_every(var_labs[names(var_labs) == vars[x]], 6, pattern = " ")
     question_label <- paste(question_label,collapse="\n")
 
-    ggplot(data = df2 %>% filter(!is.na(!!sym(vars[x]))), aes(x=.data[[vars[x]]], fill=id))+
-      geom_bar(aes(y = (..count..)/sum(..count..)), na.rm = TRUE, position = position_dodge()) +
-      geom_text(aes(label = scales::percent((..count..)/sum(..count..)), y= (..count..)/sum(..count..)), stat= "count", hjust = -0.2,
-                position = position_dodge(.9)) +
+    ggplot(data = df2 %>% filter(!is.na(!!sym(vars[x]))), aes(x=.data[[vars[x]]]))+
+      geom_bar(aes(y = (..count..)/sum(..count..)), na.rm = TRUE) +
+      geom_text(aes(label = scales::percent((..count..)/sum(..count..)), y= (..count..)/sum(..count..)), stat= "count",size=3, hjust = -0.2) +
       coord_flip() +
       scale_y_continuous(labels = scales::percent_format(accuracy = .01), limits=c(0,1))+
-      scale_fill_manual(name = "State", labels = c("Delta", "Edo"), values=c("#999999", "black")) +
       labs(title = question_label, x= NULL, y= "percent") +
       theme(axis.text.x = element_text(angle = 0, hjust = 1),
             axis.title=element_text(face="bold"),
-            plot.title = element_text(hjust = 0.5),
-            strip.text.y.left = element_text(angle = 0))})
+            plot.title = element_text(face = "bold", size=1),
+            strip.text.y.left = element_text(angle = 0),
+            text = element_text(size=20))+
+      theme_minimal() })
 }
 
 
@@ -160,16 +160,32 @@ het_together <- function(df, my_vars){
 
   df_together = do.call(rbind, by_id_df)
 
-  p_together <- ggplot(data = df_together,aes(x=question,y=Perc., fill=id)) +
-    geom_bar(stat = 'identity', position = position_dodge()) +
+  p_together <- ggplot(data = df_together,aes(x=question,y=Perc.)) +
+    geom_bar(stat = 'identity') +
     coord_flip() +
-    geom_text(aes(label=Perc.), hjust = -0.2,
-              position = position_dodge(.9)) +
+    geom_text(aes(label=paste0(Perc.,"%")), hjust = -0.2, size=3) +
     ylab("% of Respondents") + xlab("Option") + ylim(0,100)+
     scale_y_continuous(labels = function(x) paste0(x, "%"), limits=c(0,100))+
     theme(axis.text.x = element_text(colour="black"),
           axis.text.y = element_text(colour="black"),
-          plot.title = element_text(face = "bold")) +
+          plot.title = element_text(hjust = 0.5, face = "bold", size = .4),
+          text = element_text(size=20)) +
+    ggtitle(paste0(common_question))+
+    theme_minimal()
+
+
+  p_together <- ggplot(data = df_together,aes(x=question,y=Perc., fill=id)) +
+    geom_bar(stat = 'identity', position = position_dodge()) +
+    coord_flip() +
+    geom_text(aes(label=Perc.), hjust = -0.2, position = position_dodge(.9), size=3) +
+    ylab("% of Respondents") + xlab("Option") + ylim(0,100)+
+    scale_y_continuous(labels = function(x) paste0(x, "%"), limits=c(0,100))+
+    theme(axis.text.x = element_text(colour="black"),
+          axis.text.y = element_text(colour="black"),
+          plot.title = element_text(hjust = 0.5, face = "bold", size = .4),
+          text = element_text(size=20)) +
     ggtitle(paste0(common_question)) +
-    scale_fill_manual(name = "State", labels = c("Delta", "Edo"), values=c("#999999", "black"))
+    scale_fill_manual(name = "State", labels = c("Delta", "Edo"), values=c("#999999", "black"))+
+    theme_minimal()
+
 }
--- R/together_plot.R
+++ R/together_plot.R
@@ -1,9 +1,11 @@
 together_plot <- function(df, my_vars){
   together_vars <- my_vars
   prefix <- together_vars$together[[1]]
-  common_question <-  together_vars$title[[1]]
+  common_question <- split_every(together_vars$title[[1]], 6, pattern = " ")
+  common_question <- paste(common_question,collapse="\n")
 
-  together <- df %>% dplyr::select(starts_with(paste0(prefix)))  %>% names
+
+  together <- df %>% dplyr::select(together_vars$variable) %>% names
 
   df_together <- data.frame(
     question = get_label(df[,together]),
@@ -33,11 +35,12 @@ together_plot <- function(df, my_vars){
   p_together <- ggplot(data = df_together,aes(x=question,y=Perc.)) +
     geom_bar(stat = 'identity') +
     coord_flip() +
-    geom_text(aes(label=Perc.), hjust = -0.2) +
+    geom_text(aes(label=paste0(Perc.,"%")), hjust = -0.2, size=3) +
     ylab("% of Respondents") + xlab("Option") + ylim(0,100)+
+    scale_y_continuous(labels = function(x) paste0(x, "%"), limits=c(0,100))+
     theme(axis.text.x = element_text(colour="black"),
           axis.text.y = element_text(colour="black"),
-          plot.title = element_text(hjust = 0.5, face = "bold"),
+          plot.title = element_text(hjust = 0.5, face = "bold", size = .4),
           text = element_text(size=20)) +
     ggtitle(paste0(common_question))+
     theme_minimal()
